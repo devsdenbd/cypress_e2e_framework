8f0e079acf11f479e31305d7d1337fdb57bf7137
Talha Bin Ashraf <talhabinashraf@gmail.com>
2022-11-05 15:18:19 +0600

    Code Updated

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -22,14 +22,26 @@ const brandsCategory = [
 Cypress.Commands.add('validUrl', (partialUrl, fullUrl) => {
   cy.url()
     .then((value) => {
+
       cy.log('Current Url Is: ', value)
       expect(value).to.contains(partialUrl)
       expect(value).to.eq(fullUrl);
     })
+
+      cy.log('Current Url Is: ', value);
+      expect(value).to.contains(partialUrl);
+      expect(value).to.eq(fullUrl);
+    })
+  
   cy.request(fullUrl).should((response) => {
-    expect(response.status).to.eq(200)
+  expect(response.status).to.eq(200);
   });
-});
+
+  cy.request(fullUrl).should((response) => {
+    expect(response.status).to.not.eq(400);
+    cy.log("Request Time Out")
+  });
+
 
 Cypress.Commands.add('productCategory', (value) => {
   homePage.categoryItems().each((item, index, list) => {
